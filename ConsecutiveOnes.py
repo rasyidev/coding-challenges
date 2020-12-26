28005a2020fbca9320dd7ac721cb95c9c0e875f5
rasyidev <habib.rasyid11@gmail.com>
2020-12-26 21:13:49 +0700

    create .gitignore to exclude local workspace file

--- ConsecutiveOnes.py
+++ ConsecutiveOnes.py
@@ -1,15 +1,16 @@
 def findMaxConsecutiveOnes(nums):
     consecutive_count = 0
     # print("len nums:", len(nums))
-    temp = []
+    temp = 0
 
     for i in range(len(nums)):
-        if(nums[i] == 1):
+        if nums[i] == 1:
             consecutive_count +=1
-            temp.append(consecutive_count)
+            if temp > consecutive_count:
+                temp = consecutive_count
         else:
             consecutive_count = 0
-    return max(temp)
+    return temp
 res = findMaxConsecutiveOnes([1,0,1,0,1,1,1,1,0,1,1,1,1,1])
 print(res)
         
