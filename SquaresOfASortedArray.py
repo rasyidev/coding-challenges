8fd4728885355536bdf83419142600f8005a9b8d
rasyidev <habib.rasyid11@gmail.com>
2021-01-06 22:22:22 +0700

    Solve Squares of A Sorted Array problem

---
+++ SquaresOfASortedArray.py
@@ -0,0 +1,19 @@
+'''
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+'''
+
+def sortedSquares(nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+	res = list(map(lambda x: x**2, nums))
+	res.sort()
+	return res
+
+test = sortedSquares([-4,-1,0,3,10])
+print(test)
