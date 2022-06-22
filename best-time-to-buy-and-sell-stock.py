0f5f0e973e1c983441012f61abf79719e14cd085
rasyidev <habib.rasyid11@gmail.com>
2022-06-22 20:43:43 +0700

    Solve best time to buy and sell stock

--- best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock.py
@@ -7,7 +7,8 @@ def best_time_to_buy_and_sell_stock(prices):
   # mulai dari elemen ke-1 sebagai pointer right
   for i, price in enumerate(prices[1:]):
     if price < prices[l]:
-      l = i
+      l = i+1
+      print(l)
       if price - prices[l] > max_profit:
         max_profit = price - prices[l]
     else:
@@ -17,9 +18,3 @@ def best_time_to_buy_and_sell_stock(prices):
 
 print(best_time_to_buy_and_sell_stock([7,1,4,5,3,5,9,2]))
 
-# Input:
-# [2,1,4]
-# Output:
-# 2
-# Expected:
-# 3
